c2a3bd4e23e7a9f656cd4961ede3b4e705c3c33f
batman <943104990@qq.com>
2018-03-20 12:37:31 +0800

    success controller

--- src/main/java/cn/bysj/core/service/systemManage/TrsetionManageServiceImpl.java
+++ src/main/java/cn/bysj/core/service/systemManage/TrsetionManageServiceImpl.java
@@ -3,6 +3,7 @@ package cn.bysj.core.service.systemManage;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +23,7 @@ import cn.itcast.common.page.Pagination;
  */
 @Service
 @Transactional
+@Primary
 public class TrsetionManageServiceImpl implements TrsetionManageService {
 	@Autowired
 	private TrsectionMapper trsectionMapper;
