ba278e0c9d48ee32c53a62ad77629da4f93dbfe8
batman <943104990@qq.com>
2018-03-21 11:13:08 +0800

    modify wx and pageHelper configeration

--- src/main/java/cn/bysj/BysjBoot2Application.java
+++ src/main/java/cn/bysj/BysjBoot2Application.java
@@ -1,6 +1,7 @@
 package cn.bysj;
 
 import cn.bysj.core.web.captcha.JcaptchaServlet;
+import com.github.pagehelper.PageHelper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,8 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Properties;
 
 @SpringBootApplication
 @MapperScan("cn.bysj.core.mapper")
@@ -36,7 +39,7 @@ public class BysjBoot2Application extends SpringBootServletInitializer{
 		return application.sources(BysjBoot2Application.class);
 	}
 
-
+	//配置验证码
 	@Bean
 	public ServletRegistrationBean<JcaptchaServlet> testServletRegistration() {
 		ServletRegistrationBean<JcaptchaServlet> registration = new ServletRegistrationBean<>(new JcaptchaServlet());
@@ -44,4 +47,16 @@ public class BysjBoot2Application extends SpringBootServletInitializer{
 		return registration;
 	}
 
+	//配置mybatis的分页插件pageHelper
+	@Bean
+	public PageHelper pageHelper(){
+		PageHelper pageHelper = new PageHelper();
+		Properties properties = new Properties();
+		properties.setProperty("offsetAsPageNum","true");
+		properties.setProperty("rowBoundsWithCount","true");
+		properties.setProperty("reasonable","true");
+		properties.setProperty("dialect","mysql");    //配置mysql数据库的方言
+		pageHelper.setProperties(properties);
+		return pageHelper;
+	}
 }
